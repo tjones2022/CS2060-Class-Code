ed3e6ff7c89dd44eb65a86e518fd14992e06376e
debmhteach <debmh.teach@gmail.com>
2022-08-15 12:52:55 -0600

    Update GuessNumber.c

--- examples/ch01/GuessNumber/GNU/randomized_version/GuessNumber.c
+++ examples/ch01/GuessNumber/GNU/randomized_version/GuessNumber.c
@@ -9,7 +9,7 @@
 
 
 #define MIN_NUMBER 1
-const MAX_NUMBER = 100;
+const int MAX_NUMBER = 100;
 
 void guessGame(void); 
 int isCorrect(int, int); 
@@ -67,19 +67,3 @@ int isCorrect(int g, int a)
    return 0;
 } 
 
-
-
-/**************************************************************************
- * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
