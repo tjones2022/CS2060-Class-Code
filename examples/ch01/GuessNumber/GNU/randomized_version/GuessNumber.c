60cbf671071062e5be54b0b9bdacfed18c377c3b
debmhteach <debmh.teach@gmail.com>
2023-01-09 16:24:08 -0700

    Update GuessNumber.c

--- examples/ch01/GuessNumber/GNU/randomized_version/GuessNumber.c
+++ examples/ch01/GuessNumber/GNU/randomized_version/GuessNumber.c
@@ -3,60 +3,82 @@
 * between 1 and 1000 for the user to guess.
 */
 
+
+// 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
 
-
+// 
 #define MIN_NUMBER 1
+
+//
 const int MAX_NUMBER = 100;
 
+//
 void guessGame(void); 
 bool isCorrect(int, int); 
 
 
 int main(void)
 {
+    //
    srand(time(0));
+
+   //
    guessGame(); 
 
+   //
    return 0;
-} // end main
+}
 
-
+//
 void guessGame(void)
 {
+    //
    int answer = 0; 
    int guess = 0; 
    int response = 0; 
 
    do {
+
+       //
       answer = MIN_NUMBER + rand() % MAX_NUMBER;
 
+
+      //
       printf("Hello, I have a number between %d and %d .\n" 
           "Can you guess my number?\n"
            "Please type your first guess.", MIN_NUMBER, MAX_NUMBER);
 
-      printf("%s", "? ");
-      scanf("%d", &guess);
+      //
 
-      while (!isCorrect(guess, answer)) 
-         scanf("%d", &guess);
-
+      //
+      do
+      {
+          //
+          scanf("%d", &guess);
+      } while (!isCorrect(guess, answer));
+         
+      //
       puts("\nExcellent! You guessed the number!\n"
          "Would you like to play again?");
-      printf("%s", "Please type ( 1=yes, 2=no )? ");
+
+      printf("%s", "Please type ( 1=yes, 2=no ) ");
       scanf("%d", &response);
 
       puts("");
    } while (response == 1);
 } 
 
-
+//
 bool isCorrect(int g, int a)
 {
+    //
     bool found = false;
+
+    //
     if (g == a){
         found = true;
     }else  if (g < a)
@@ -64,6 +86,7 @@ bool isCorrect(int g, int a)
    else
       printf( "%s", "Too high. Try again.\n? " );
 
+    //
    return found;
 } 
 
